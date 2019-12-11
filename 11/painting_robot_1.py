b5c39792b56152e23e8e9d1e039079d1eaf67972
Marcin Peczkowski <marcin.peczkowski@mobica.com>
2019-12-11 10:54:02 +0100

    Day 11

---
+++ 11/painting_robot_1.py
@@ -0,0 +1,62 @@
+import int_comp as ic
+from collections import namedtuple
+
+Position = namedtuple("Position", "x y")
+
+memory = [int(i) for i in input().split(',')]
+memory.extend([0]*1000)
+
+comp = ic.Computer(memory, 0)
+
+directions = ["up", "right", "down", "left"]
+dir_idx = 0
+
+tiles = {Position(0,0) : 0} # position : color
+colored_tiles = set()
+colored_tiles_list = []
+robot_pos = Position(0, 0)
+
+while True:
+   outputs, state = comp.compute_result()
+   color, direction = outputs
+
+   # paint tile
+   current_color = tiles.get(robot_pos, 0)
+   if current_color != color:
+      tiles[robot_pos] = color
+      colored_tiles.add(robot_pos)
+      print("Painted", robot_pos, color)
+   
+   # adjust robot's direction
+   if direction == 0: # turn left 90 degress
+      dir_idx -= 1
+      if dir_idx < 0:
+         dir_idx = len(directions) - 1
+   else:              # turn right 90 degrees
+      dir_idx += 1
+      if dir_idx == len(directions):
+         dir_idx = 0
+
+   # move robot
+   dir_ = directions[dir_idx]
+   prev_pos = robot_pos
+   if dir_ == "up":
+      robot_pos = Position(robot_pos.x, robot_pos.y+1)
+   elif dir_ == "down":
+      robot_pos = Position(robot_pos.x, robot_pos.y-1)
+   elif dir_ == "right":
+      robot_pos = Position(robot_pos.x+1, robot_pos.y)
+   elif dir_ == "left":
+      robot_pos = Position(robot_pos.x-1, robot_pos.y)
+
+   # print("Moving", dir_, "from", prev_pos, "to", robot_pos)
+
+   input_color = 0
+   if robot_pos in tiles.keys():
+      input_color = tiles[robot_pos]
+   comp.set_input(input_color)
+
+   if state == ic.State.FINISHED:
+      break
+
+print("Colored tiles:", len(colored_tiles))
